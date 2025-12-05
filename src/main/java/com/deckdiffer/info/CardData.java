71a5e797f97faf677cddf4b8d30a13beb7b53797
Michael Bai <michaelbai30@gmail.com>
2025-12-05 15:02:52 -0500

    Added fields for converted mana cost (cmc) and pip counts

--- src/main/java/com/deckdiffer/info/CardData.java
+++ src/main/java/com/deckdiffer/info/CardData.java
@@ -1,20 +1,22 @@
 // Model for a given card
 package com.deckdiffer.info;
-
+import java.util.Map;
 import java.util.List;
 import org.json.JSONObject;
 
 public class CardData {
-    public JSONObject json;
-    public List<String> types;
-    public String primaryType;
-    public List<String> colors;
-    public String colorCategory;
-    public double price;
-    public String imageUrl;
-    public String scryfallUrl;
+    public final JSONObject json;
+    public final List<String> types;
+    public final String primaryType;
+    public final List<String> colors;
+    public final String colorCategory;
+    public final double price;
+    public final String imageUrl;
+    public final String scryfallUrl;
+    public final double cmc;
+    public final Map<String, Integer> pipCounts;
 
-    public CardData(JSONObject json, List<String> types, String primaryType, List<String> colors, String colorCategory, double price, String imageUrl, String scryfallUrl){
+    public CardData(JSONObject json, List<String> types, String primaryType, List<String> colors, String colorCategory, double price, String imageUrl, String scryfallUrl, double cmc, Map<String, Integer> pipCounts){
         this.json = json;
         this.types = types;
         this.primaryType = primaryType;
@@ -23,5 +25,7 @@ public class CardData {
         this.price = price;
         this.imageUrl = imageUrl;
         this.scryfallUrl = scryfallUrl;
+        this.cmc = cmc;
+        this.pipCounts = pipCounts;
     }
 }
