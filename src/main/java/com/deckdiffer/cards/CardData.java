23312813b47089dd61f60af5065148adcf4a066c
Michael Bai <michaelbai30@gmail.com>
2025-12-08 10:43:34 -0500

    Improved immutability in CardData and refined docs

--- src/main/java/com/deckdiffer/cards/CardData.java
+++ src/main/java/com/deckdiffer/cards/CardData.java
@@ -1,16 +1,18 @@
-/* CardData.java: Model for a given card */
+/* CardData.java: 
+- Model for a given card
+- Populated after JSON is downloaded from Scryfall and relevant fields are extracted*/
 
 package com.deckdiffer.cards;
 import java.util.Map;
 import java.util.List;
 import org.json.JSONObject;
 
-public class CardData {
+public final class CardData {
     public final JSONObject json; // Raw Scryfall JSON response
     public final List<String> types; // All card types and subtypes (ex: Creature, Artifact, Enchantment)
     public final String primaryType; // Main type used for grouping
-    public final List<String> colors; // WUBRG color identity of the card
-    public final String colorCategory; // High-level color category
+    public final List<String> colors; // WUBRG color identity of the card, ex: {"W", "U", "B"}
+    public final String colorCategory; // String representing color category, ex: "WUB"
     public final double price; // Price of the card in USD according to Scryfall
     public final String imageUrl; // Artwork image URL for rendering
     public final String scryfallUrl; // Link to the card on Scryfall
@@ -19,14 +21,14 @@ public class CardData {
 
     public CardData(JSONObject json, List<String> types, String primaryType, List<String> colors, String colorCategory, double price, String imageUrl, String scryfallUrl, double cmc, Map<String, Integer> pipCounts){
         this.json = json;
-        this.types = types;
+        this.types = List.copyOf(types);
         this.primaryType = primaryType;
-        this.colors = colors;
+        this.colors = List.copyOf(colors);
         this.colorCategory = colorCategory;
         this.price = price;
         this.imageUrl = imageUrl;
         this.scryfallUrl = scryfallUrl;
         this.cmc = cmc;
-        this.pipCounts = pipCounts;
+        this.pipCounts = Map.copyOf(pipCounts);
     }
 }
